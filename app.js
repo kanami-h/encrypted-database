d35084482b54383c48d7d318fd1495860a81d87f
Kanami <kanami.h217@gmail.com>
2021-12-20 21:34:25 -0500

    added md5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const { Schema } = mongoose;
 
 const app = express();
@@ -18,12 +18,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
-// it's important that we include this plugin before the model
 
 const User = mongoose.model("User", userSchema);
 
@@ -43,7 +37,7 @@ app.post("/register", (req, res) => {
   // username, password
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save((err) => {
@@ -57,17 +51,17 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, (err, foundUser) => {
-    if (!err) {
+    if (err) {
+      console.log(err);
+    } else {
       if (foundUser) {
         if (foundUser.password === password) {
           res.render("secrets");
         }
       }
-    } else {
-      console.log("Your email or password doesn't match. Try again");
     }
   });
 });
